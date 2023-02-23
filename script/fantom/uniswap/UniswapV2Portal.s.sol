472b99ac6be8a4d1472e3c83672baca6571d6a0e
SuhailG <github@suhailg.me>
2023-02-23 07:37:26 -0700

    Create UniswapV2Portal.s.sol

---
+++ script/fantom/uniswap/UniswapV2Portal.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "forge-std/Script.sol";
+import { UniswapV2Portal } from
+    "../../../src/uniswap/UniswapV2Portal.sol";
+
+import { Addresses } from "../../constants/Addresses.sol";
+
+contract UniswapV2PortalDeployer is Script {
+    Addresses addresses = new Addresses();
+
+    function run() external {
+        string memory network = "Fantom";
+
+        string memory deployerMnemonic = vm.envString("MNEMONIC");
+        uint256 deployerPrivateKey = vm.deriveKey(deployerMnemonic, 0);
+        vm.startBroadcast(deployerPrivateKey);
+
+        console2.log("Deployer address", vm.addr(deployerPrivateKey));
+
+        UniswapV2Portal uniswapV2Portal =
+            new UniswapV2Portal(addresses.get(network, "admin"));
+        console2.log(
+            "Deployed UniswapV2Portal at", address(uniswapV2Portal)
+        );
+
+        vm.stopBroadcast();
+    }
+}
