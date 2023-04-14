01fa07ca24de148c7b24a28f1531de35e32c9175
SuhailG <github@suhailg.me>
2023-04-14 05:09:30 -0600

    Deploy router and multicall to avalanche

---
+++ script/avalanche/portals/router/PortalsRouter.s.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "forge-std/Script.sol";
+import { PortalsMulticall } from
+    "../../../../src/portals/multicall/PortalsMulticall.sol";
+import { PortalsRouter } from
+    "../../../../src/portals/router/PortalsRouter.sol";
+
+import { Addresses } from "../../../constants/Addresses.sol";
+
+contract PortalsRouterDeployer is Script {
+    Addresses addresses = new Addresses();
+
+    function run() external {
+        string memory network = "Avalanche";
+
+        string memory deployerMnemonic = vm.envString("MNEMONIC");
+        uint256 deployerPrivateKey = vm.deriveKey(deployerMnemonic, 0);
+        vm.startBroadcast(deployerPrivateKey);
+
+        console2.log("Deployer address", vm.addr(deployerPrivateKey));
+
+        PortalsMulticall multicall = new PortalsMulticall();
+        console2.log(
+            "Deployed PortalsMulticall at", address(multicall)
+        );
+
+        PortalsRouter router =
+        new PortalsRouter(addresses.get(network, "admin"), addresses.get(network, "collector"), address(multicall));
+
+        console2.log("Deployed PortalsRouter at", address(router));
+
+        vm.stopBroadcast();
+    }
+}
--- script/constants/Addresses.sol
+++ script/constants/Addresses.sol
@@ -5,19 +5,29 @@ contract Addresses {
     mapping(string => address) private Ethereum;
     mapping(string => address) private Polygon;
     mapping(string => address) private Fantom;
+    mapping(string => address) private Avalanche;
+
+    error NoAddress(string network, string name);
 
     constructor() {
         Polygon["admin"] = 0x5c883bAef73C7F0e9C5Ee9d6DfaCCd3ed00ACf26;
         Ethereum["admin"] = 0x7cFecFBA73D62125F2eef82A0E0454e4000935bE;
         Fantom["admin"] = 0xC585C45D6538DABc58E7740a07e840a679AB872B;
+        Avalanche["admin"] =
+            0xB0f6bC7b4D996cC483EA3578585Fa74E71280C53;
+
         Polygon["collector"] =
             0x508ee1b661c7DeE089A5b5c3fD234f1058F03c38;
         Ethereum["collector"] =
             0xFBD4C3D8bE6B15b7cf428Db2838bb44C0054fCd2;
         Fantom["collector"] =
             0x9144439a1d4d5Fb371C491101045815F32150444;
+        Avalanche["collector"] =
+            0xdDe0bb7c1F953667538bBf11798F0BbbF90Fa313;
+
         Ethereum["zeroEx"] =
             0xDef1C0ded9bec7F1a1670819833240f027b25EfF;
+
         Ethereum["WETH"] = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     }
 
@@ -32,8 +42,7 @@ contract Addresses {
         ) {
             entity = Polygon[name];
             require(entity != address(0), "Polygon address not found");
-        }
-        if (
+        } else if (
             keccak256(abi.encodePacked(network))
                 == keccak256(abi.encodePacked("Ethereum"))
         ) {
@@ -41,13 +50,22 @@ contract Addresses {
             require(
                 entity != address(0), "Ethereum address not found"
             );
-        }
-        if (
+        } else if (
             keccak256(abi.encodePacked(network))
                 == keccak256(abi.encodePacked("Fantom"))
         ) {
             entity = Fantom[name];
             require(entity != address(0), "Fantom address not found");
+        } else if (
+            keccak256(abi.encodePacked(network))
+                == keccak256(abi.encodePacked("Avalanche"))
+        ) {
+            entity = Fantom[name];
+            require(
+                entity != address(0), "Avalanche address not found"
+            );
+        } else {
+            revert NoAddress(network, name);
         }
     }
 }
