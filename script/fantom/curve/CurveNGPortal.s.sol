9f3938851cf8c6e0c7ea2960b3def295c038ecc1
SuhailG <github@suhailg.me>
2024-03-17 21:18:04 -0600

    Deploy and verify CurveNGPortal on all networks

---
+++ script/fantom/curve/CurveNGPortal.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import "forge-std/Script.sol";
+import { CurveNGPortal } from "../../../src/curve/CurveNGPortal.sol";
+
+import { Addresses } from "../../constants/Addresses.sol";
+
+contract CurveNGPortalDeployer is Script {
+    Addresses addresses = new Addresses();
+
+    function run() external {
+        string memory network = "Fantom";
+
+        string memory deployerMnemonic = vm.envString("MNEMONIC");
+        uint256 deployerPrivateKey = vm.deriveKey(deployerMnemonic, 0);
+        vm.startBroadcast(deployerPrivateKey);
+
+        console2.log("Deployer address", vm.addr(deployerPrivateKey));
+
+        CurveNGPortal curveNGPortal =
+            new CurveNGPortal(addresses.get(network, "admin"));
+        console2.log(
+            "Deployed CurveNGPortal at", address(curveNGPortal)
+        );
+
+        vm.stopBroadcast();
+    }
+}
