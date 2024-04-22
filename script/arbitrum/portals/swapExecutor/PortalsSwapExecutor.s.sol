1e76ef4f11f0c262f17578e6924c13699d00ad16
SuhailG <github@suhailg.me>
2024-04-22 08:28:13 -0600

    Deploy on Ethereum, BSC, Optimism, Arbitrum, Base

---
+++ script/arbitrum/portals/swapExecutor/PortalsSwapExecutor.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import "forge-std/Script.sol";
+import { PortalsSwapExecutor } from
+    "../../../../src/portals/swapExecutor/PortalsSwapExecutor.sol";
+
+import { Addresses } from "../../../constants/Addresses.sol";
+
+contract PortalsSwapExecutorDeployer is Script {
+    Addresses addresses = new Addresses();
+
+    function run() external {
+        string memory network = "Arbitrum";
+
+        string memory deployerMnemonic = vm.envString("MNEMONIC");
+        uint256 deployerPrivateKey = vm.deriveKey(deployerMnemonic, 0);
+        vm.startBroadcast(deployerPrivateKey);
+
+        console2.log("Deployer address", vm.addr(deployerPrivateKey));
+
+        PortalsSwapExecutor executor =
+            new PortalsSwapExecutor(addresses.get(network, "admin"));
+        console2.log(
+            "Deployed PortalsSwapExecutor at", address(executor)
+        );
+
+        vm.stopBroadcast();
+    }
+}
