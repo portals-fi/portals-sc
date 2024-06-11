ab1f7dd91093196c4e9793888f475ba2b4fba43e
T <52665225+valamidev@users.noreply.github.com>
2024-06-11 17:01:58 +0200

    Add Gyroscope utility contract

---
+++ src/balancer/BalancerGyroscrope.sol
@@ -0,0 +1,158 @@
+/// SPDX-License-Identifier: GPL-3.0
+
+/// Copyright (C) 2023 Portals.fi
+
+pragma solidity 0.8.19;
+
+import { ERC20 } from "solmate/tokens/ERC20.sol";
+import { SafeTransferLib } from "solmate/utils/SafeTransferLib.sol";
+import { Owned } from "solmate/auth/Owned.sol";
+import { Pausable } from
+    "openzeppelin-contracts/security/Pausable.sol";
+import { IBalancerVault } from "./interface/IBalancerVault.sol";
+
+contract BalancerGyroscopePortal is Owned, Pausable {
+    using SafeTransferLib for address;
+    using SafeTransferLib for ERC20;
+
+    constructor(address admin) Owned(admin) { }
+
+    function portalIn(
+        address vault,
+        bytes32 poolId,
+        address[] calldata assets,
+        uint256 bltOutAmount,
+        address recipient
+    ) external payable whenNotPaused {
+        uint256[] memory maxAmountsIn = new uint256[](assets.length);
+
+        for (uint256 i = 0; i < assets.length; i++) {
+            maxAmountsIn[i] = ERC20(assets[i]).balanceOf(msg.sender);
+
+            _transferFromCaller(assets[i], maxAmountsIn[i]);
+            _approve(assets[i], vault);
+        }
+
+        // ALL_TOKENS_IN_FOR_EXACT_BPT_OUT = 3
+        bytes memory userData = abi.encode(3, bltOutAmount);
+
+        IBalancerVault(vault).joinPool(
+            poolId,
+            address(this),
+            recipient,
+            IBalancerVault.JoinPoolRequest({
+                assets: assets,
+                maxAmountsIn: maxAmountsIn,
+                userData: userData,
+                fromInternalBalance: false
+            })
+        );
+
+        // Return dust
+        for (uint256 i = 0; i < assets.length; i++) {
+            uint256 dustBalance =
+                ERC20(assets[i]).balanceOf(address(this));
+
+            ERC20(assets[i]).transfer(recipient, dustBalance);
+        }
+    }
+
+    /// @notice Remove liquidity from Balancer V2 like pools into network tokens/ERC20 tokens
+    /// @param inputToken The Balancer V2 like pool address (i.e. the LP token address)
+    /// @param inputAmount The quantity of inputToken to Portal out
+    /// @param vault The Balancer V2 like vault to be used for removing liquidity
+    /// @param poolId The ID of the pool to remove liquidity from
+    /// @param assets The assets in the pool
+    /// @param recipient The recipient of the withdrawn tokens
+    function portalOut(
+        address vault,
+        bytes32 poolId,
+        address inputToken,
+        uint256 inputAmount,
+        address[] calldata assets,
+        uint256[] calldata minAmountsOut,
+        address payable recipient
+    ) external payable whenNotPaused {
+        _transferFromCaller(inputToken, inputAmount);
+
+        // EXACT_BPT_IN_FOR_TOKENS_OUT = 1
+        bytes memory userData = abi.encode(1, inputAmount);
+
+        _approve(inputToken, address(vault));
+
+        IBalancerVault(vault).exitPool(
+            poolId,
+            address(this),
+            payable(recipient),
+            IBalancerVault.ExitPoolRequest({
+                assets: assets,
+                minAmountsOut: minAmountsOut,
+                userData: userData,
+                toInternalBalance: false
+            })
+        );
+    }
+
+    /// @notice Transfers tokens or the network token from the caller to this contract
+    /// @param token The address of the token to transfer (address(0) if network token)
+    /// @param quantity The quantity of tokens to transfer from the caller
+    /// @return The quantity of tokens or network tokens transferred from the caller to this contract
+    function _transferFromCaller(address token, uint256 quantity)
+        internal
+        virtual
+        returns (uint256)
+    {
+        if (token == address(0)) {
+            require(msg.value != 0, "Invalid msg.value");
+            return msg.value;
+        }
+
+        require(
+            quantity != 0 && msg.value == 0,
+            "Invalid quantity or msg.value"
+        );
+        ERC20(token).safeTransferFrom(
+            msg.sender, address(this), quantity
+        );
+
+        return quantity;
+    }
+
+    /// @notice Approve a token for spending with infinite allowance
+    /// @param token The ERC20 token to approve
+    /// @param spender The spender of the token
+    function _approve(address token, address spender) internal {
+        ERC20 _token = ERC20(token);
+        if (_token.allowance(address(this), spender) == 0) {
+            _token.safeApprove(spender, type(uint256).max);
+        }
+    }
+
+    /// @dev Pause the contract
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    /// @dev Unpause the contract
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    /// @notice Recovers stuck tokens
+    /// @param tokenAddress The address of the token to recover (address(0) if ETH)
+    /// @param tokenAmount The quantity of tokens to recover
+    /// @param to The address to send the recovered tokens to
+    function recoverToken(
+        address tokenAddress,
+        uint256 tokenAmount,
+        address to
+    ) external onlyOwner {
+        if (tokenAddress == address(0)) {
+            to.safeTransferETH(tokenAmount);
+        } else {
+            ERC20(tokenAddress).safeTransfer(to, tokenAmount);
+        }
+    }
+
+    receive() external payable { }
+}
---
+++ test/ethereum/balancer/Balancerv2Gyro.t.sol
@@ -0,0 +1,188 @@
+/// SPDX-License-Identifier: GPL-3.0
+
+/// Copyright (C) 2023 Portals.fi
+
+/// @author Portals.fi
+/// @notice This contract tests PortalsRouter.sol
+
+pragma solidity 0.8.19;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "forge-std/console2.sol";
+
+import { PortalsRouter } from
+    "../../../src/portals/router/PortalsRouter.sol";
+import { PortalsMulticall } from
+    "../../../src/portals/multicall/PortalsMulticall.sol";
+import { BalancerGyroscopePortal } from
+    "../../../src/balancer/BalancerGyroscrope.sol";
+import { IPortalsRouter } from
+    "../../../src/portals/router/interface/IPortalsRouter.sol";
+import { IPortalsMulticall } from
+    "../../../src/portals/multicall/interface/IPortalsMulticall.sol";
+
+import { Quote } from "../../utils/Quote/Quote.sol";
+import { IQuote } from "../../utils/Quote/interface/IQuote.sol";
+import { SigUtils } from "../../utils/SigUtils.sol";
+import { Addresses } from "../../../script/constants/Addresses.sol";
+
+import { ERC20 } from "solmate/tokens/ERC20.sol";
+
+contract BalancerGyroTest is Test {
+    uint256 mainnetFork =
+        vm.createSelectFork(vm.envString("MAINNET_RPC_URL"));
+
+    uint256 internal ownerPrivateKey = 0xDAD;
+    uint256 internal userPrivateKey = 0xB0B;
+    uint256 internal collectorPivateKey = 0xA11CE;
+    uint256 internal adversaryPivateKey = 0xC0C;
+    uint256 internal partnerPivateKey = 0xABE;
+
+    address internal owner = vm.addr(ownerPrivateKey);
+    address internal user = vm.addr(userPrivateKey);
+    address internal collector = vm.addr(collectorPivateKey);
+    address internal adversary = vm.addr(adversaryPivateKey);
+    address internal partner = vm.addr(partnerPivateKey);
+
+    address internal USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address internal GYD = 0xe07F9D810a48ab5c3c914BA3cA53AF14E4491e8A; // Gyro Dollar
+    address internal ETH = address(0);
+
+    bytes32 internal poolId =
+        0xc2aa60465bffa1a88f5ba471a59ca0435c3ec5c100020000000000000000062c;
+
+    address internal gyroPool =
+        0xC2AA60465BfFa1A88f5bA471a59cA0435c3ec5c1;
+    address internal BalancerV2Vault =
+        0xBA12222222228d8Ba445958a75a0704d566BF2C8;
+
+    PortalsMulticall public multicall = new PortalsMulticall();
+
+    PortalsRouter public router = new PortalsRouter(owner, multicall);
+
+    Addresses public addresses = new Addresses();
+
+    BalancerGyroscopePortal public gyroPortal =
+        new BalancerGyroscopePortal(owner);
+
+    Quote public quote = new Quote();
+
+    function setUp() public {
+        startHoax(user);
+    }
+
+    function test_PortalIn() public {
+        address token0 = USDC;
+        address token1 = GYD;
+        address pool = gyroPool;
+        address recipient = user;
+        uint256 amount0 = 10 ** 6;
+        uint256 amount1 = 1 ether;
+
+        // Mimic sending Tokens to user for the purpose of this test
+        deal(token0, recipient, amount0);
+        deal(token1, recipient, amount1);
+
+        // Check the initial WBNB balance of the user
+        assertEq(ERC20(token0).balanceOf(recipient), amount0);
+        assertEq(ERC20(token1).balanceOf(recipient), amount1);
+
+        // Approve the amount to spend by the portal
+        ERC20(token0).approve(address(gyroPortal), amount0);
+        ERC20(token1).approve(address(gyroPortal), amount1);
+
+        address[] memory tokens = new address[](2);
+
+        tokens[0] = token0;
+        tokens[1] = token1;
+
+        // Perform the portalIn operation
+        gyroPortal.portalIn(
+            BalancerV2Vault, poolId, tokens, 1e18, recipient
+        );
+
+        uint256 finalBalance = ERC20(pool).balanceOf(recipient);
+
+        uint256 finalBalanceToken0 =
+            ERC20(token0).balanceOf(address(gyroPortal));
+        uint256 finalBalanceToken1 =
+            ERC20(token1).balanceOf(address(gyroPortal));
+
+        assertTrue(
+            finalBalanceToken0 == 0 || finalBalanceToken1 == 0,
+            "One of the tokens should be 0 after portalIn"
+        );
+
+        uint256 inputBalance0 =
+            ERC20(token0).balanceOf(address(recipient));
+        uint256 inputBalance1 =
+            ERC20(token1).balanceOf(address(recipient));
+
+        console2.log("inputBalance0", inputBalance0);
+        console2.log("inputBalance1", inputBalance1);
+
+        assertTrue(
+            finalBalance > 99_000,
+            "Balance should be higher after portalIn"
+        );
+    }
+
+    function test_PortalOut() public {
+        address token0 = USDC;
+        address token1 = GYD;
+        address pool = gyroPool;
+        address payable recipient = payable(user);
+        uint256 lpAmount = 1 ether;
+
+        // Mimic sending Tokens to user for the purpose of this test
+        deal(pool, recipient, lpAmount);
+
+        // Check the initial WBNB balance of the user
+        assertEq(ERC20(pool).balanceOf(recipient), lpAmount);
+
+        // Approve the amount to spend by the portal
+        ERC20(pool).approve(address(gyroPortal), lpAmount);
+
+        uint256[] memory minAmountsOut = new uint256[](2);
+
+        minAmountsOut[0] = 0;
+        minAmountsOut[1] = 0;
+
+        address[] memory assets = new address[](2);
+
+        assets[0] = token0;
+        assets[1] = token1;
+
+        // Perform the portalIn operation
+        gyroPortal.portalOut(
+            BalancerV2Vault,
+            poolId,
+            pool,
+            lpAmount,
+            assets,
+            minAmountsOut,
+            recipient
+        );
+
+        uint256 finalBalance = ERC20(pool).balanceOf(recipient);
+
+        uint256 outputBalance0 =
+            ERC20(token0).balanceOf(address(recipient));
+        uint256 outputBalance1 =
+            ERC20(token1).balanceOf(address(recipient));
+
+        console2.log("outputBalance0", outputBalance0);
+        console2.log("outputBalance1", outputBalance1);
+
+        assertTrue(
+            outputBalance0 > 0 || outputBalance1 > 0,
+            "One of the tokens should be 0 after portalIn"
+        );
+
+        assertTrue(
+            finalBalance == 0,
+            "Balance should be zero after PortalOut"
+        );
+    }
+}
